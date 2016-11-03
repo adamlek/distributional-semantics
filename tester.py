aed35f6e9256570b413f65e5df8387b421e7eef0
adamlek <df90aqx@gmail.com>
2016-11-03 11:45:17 +0100

    1

--- tester.py
+++ tester.py
@@ -36,10 +36,11 @@ def new_save():
     plotting = False
     teststuff = True
 
-    dataset1 = ['/home/usr1/git/dist_data/test_doc_1.txt', '/home/usr1/git/dist_data/test_doc_2.txt', '/home/usr1/git/dist_data/test_doc_3.txt', '/home/usr1/git/dist_data/test_doc_4.txt', '/home/usr1/git/dist_data/test_doc_5.txt', '/home/usr1/git/dist_data/test_doc_6.txt']
+#    dataset1 = ['/home/usr1/git/dist_data/test_doc_5.txt']
 #    dataset1 = ['/home/usr1/git/dist_data/austen-emma.txt', '/home/usr1/git/dist_data/austen-persuasion.txt', '/home/usr1/git/dist_data/austen-sense.txt']
-#    dataset1 = ['/home/usr1/git/dist_data/reut1.txt']
-#    dataset1 = ['/home/usr1/git/dist_data/formatted2.txt', '/home/usr1/git/dist_data/reut1.txt', '/home/usr1/git/dist_data/reut2.txt']
+#    dataset1 = ['/home/usr1/git/dist_data/reut1.txt', '/home/usr1/git/dist_data/reut2.txt']
+    dataset1 = ['/home/usr1/git/dist_data/formatted2.txt', '/home/usr1/git/dist_data/reut1.txt', '/home/usr1/git/dist_data/reut2.txt']
+#    dataset1 = ['/home/usr1/git/dist_data/formatted2.txt', '/home/usr1/git/dist_data/test_doc_5.txt', '/home/usr1/git/dist_data/test_doc_1.txt', '/home/usr1/git/dist_data/test_doc_3.txt']
 
 #DATAREADER
 ##################################################
@@ -47,7 +48,7 @@ def new_save():
     #read the file dataset1 and output all sentences, all words, and information about word count/documents
     sentences, vocabulary, documents = dr.preprocess_data(dataset1)
 
-#    print(len(sentences))
+    print(len(sentences))
 #    for sent in sentences:
 #        print(sent, '\n')
 
@@ -60,20 +61,24 @@ def new_save():
     print('reading file done')
 ##################################################
 
+    #SETTINGS
+    w, t, s, d, r = 1, 0, 2, 3072, 6
+    print(w, t, s, d, r)
+
+
 ##RANDOMVECTORIZER
 ###################################################
-    rv = RandomVectorizer(dimensions=1024, random_elements=4)
+    rv = RandomVectorizer(dimensions=d, random_elements=r)
     #create word and random vectors for the strings in vocabulary
     vectors = rv.vocabulary_vectorizer(vocabulary)
+
     print('vectoring done')
 ###################################################
 
 ##WEIGHTER
 ###################################################
-    w, t, s = 1, 0, 2
-    print(w, t, s)
     #init Weighter, with scheme 0 and don't do idf
-    tr = TermRelevance(documents, scheme=w, doidf=True, smooth_idf=True)
+    tr = TermRelevance(documents, scheme=w, doidf=True, smooth_idf=False)
 
     #weight the dictionary of vectors
     vectors = tr.weight(vectors)
@@ -87,28 +92,20 @@ def new_save():
     cont5 = Contexter(vectors, contexttype=t, window=5, context_scope=s)
     cont10 = Contexter(vectors, contexttype=t, window=10, context_scope=s)
 
-    
     vector_vocabulary1 = cont1.process_data(sentences)
     vector_vocabulary5 = cont5.process_data(sentences)
     vector_vocabulary10 = cont10.process_data(sentences)
     #poor computah :()
 
-    cont_dict = cont1.vocabt
-#    print(cont_dict)
-#    for v in cont_dict:
-#        print(v, cont_dict[v])
-    ###PPMI of co-occurence
-    xpmi = cont1.PPMImatrix(cont_dict, documents)
-#    for v in pmi:
-#        print(v)
-#        for n in pmi[v]:
-#            print(n, pmi[v][n])
-    
-#    print(vocab)
-#    for i, m in enumerate(ppmi):
-#        print(vocab[i])
-#        print(m, '\n')
-#    print(cc)
+#    cont_dict1 = cont1.vocabt
+#    cont_dict5 = cont5.vocabt
+#    cont_dict10 = cont10.vocabt
+
+#    ###PPMI of co-occurence
+#    xpmi1 = cont1.PPMImatrix(cont_dict1, documents)
+#    xpmi5 = cont5.PPMImatrix(cont_dict5, documents)
+#    xpmi10 = cont10.PPMImatrix(cont_dict10, documents)
+
 
     print('reading contexts done')
 ###################################################
@@ -125,9 +122,12 @@ def new_save():
 ###################################################
 
     #Initialize similarity class
-    sim1 = Similarity(vector_vocabulary1, pmi = xpmi)
-    sim5 = Similarity(vector_vocabulary5, pmi = xpmi)
-    sim10 = Similarity(vector_vocabulary10, pmi = xpmi)
+#    sim1 = Similarity(vector_vocabulary1, pmi = xpmi1)
+#    sim5 = Similarity(vector_vocabulary5, pmi = xpmi5)
+#    sim10 = Similarity(vector_vocabulary10, pmi = xpmi10)
+    sim1 = Similarity(vector_vocabulary1)
+    sim5 = Similarity(vector_vocabulary5)
+    sim10 = Similarity(vector_vocabulary10)
 
     with open('/home/usr1/git/dist_data/combined.csv') as f:
         with open('/home/usr1/git/dist_data/combined1.csv', 'w') as res:
@@ -137,7 +137,7 @@ def new_save():
                 s1 = sim1.cosine_similarity(ln[0], ln[1])
                 s5 = sim5.cosine_similarity(ln[0], ln[1])
                 s10 = sim10.cosine_similarity(ln[0], ln[1])
-                
+
                 try:
                     o = float(s1)
                     oo = float(s5)
@@ -158,7 +158,7 @@ def new_save():
             riv10 = []
             for i, ln in enumerate(f):
                 ln = ln.rstrip().split(',')
-#                print(ln)
+
                 humanv.append(float(ln[2]))
                 riv1.append(float(ln[3]))
                 riv5.append(float(ln[4]))
@@ -167,11 +167,11 @@ def new_save():
 
         print(len(humanv), len(riv1), len(riv5), len(riv10))
         print(st.stats.spearmanr(humanv, riv1))
-        print(st.pearsonr(humanv,riv1), '1\n')
+        print('pearson r, p-val', st.pearsonr(humanv,riv1), '1\n')
         print(st.stats.spearmanr(humanv, riv5))
-        print(st.pearsonr(humanv,riv5), '5\n')
+        print('pearson r, p-val', st.pearsonr(humanv,riv5), '5\n')
         print(st.stats.spearmanr(humanv, riv10))
-        print(st.pearsonr(humanv,riv10), '10\n')
+        print('pearson r, p-val', st.pearsonr(humanv,riv10), '10\n')
 
 #TSNE PLOTTING
 ##################################################
@@ -183,13 +183,7 @@ def new_save():
                 ar.append(vector_vocabulary5[v])
                 lbs.append(v)
 
-#        lbs = ['the', 'when', 'speak', 'run', 'high', 'flow', 'love', 'he', 'and', 'where', 'talk', 'walk', 'low', 'water', 'hate', 'she']
-#        for word in lbs:
-#            ar.append(vector_vocabulary5[word])
-
-        arrs = np.array(ar)
-        Y = tsne.tsne(arrs, 2, 50, 20.0)
-#        print(Y)
+        Y = tsne.tsne(np.array(ar), 2, 50, 20.0)
         fig, ax = plt.subplots()
         ax.scatter(Y[:,0], Y[:,1], 20)
 
